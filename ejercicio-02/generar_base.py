75e0793caf01e7fb5ff5b5ae4ce6fa31598f2818
joechuro1206 <joech1206@gmail.com>
2024-05-18 20:13:43 -0500

    Generar base de datos

---
+++ ejercicio-02/generar_base.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+
+# se genera en enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine('sqlite:///dbpaises.db')
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+
+class Pais(Base):
+    __tablename__ = 'lospaises'
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    capital = Column(String)
+    continente = Column(String)
+    dial = Column(String)
+    geonameID = Column(Integer)
+    itu = Column(String)
+    lenguajes = Column(String)
+    esIndependiente = Column(String)
+
+Base.metadata.create_all(engine)
